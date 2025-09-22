0b85daa6cc4a4682e7f66abc64c1d61a9d7d7b3c
Charles <975047+pycharles@users.noreply.github.com>
2025-09-22 11:03:51 -0400

    use cloudfront

--- charliechat-terraform/route53.tf
+++ charliechat-terraform/route53.tf
@@ -8,15 +8,15 @@ resource "aws_route53_zone" "charlesob" {
   }
 }
 
-# A record with fqdn alias for root domain pointing to API Gateway
+# A record with fqdn alias for root domain pointing to CloudFront
 resource "aws_route53_record" "charlesob_root" {
   zone_id = aws_route53_zone.charlesob.zone_id
   name    = "charlesob.com"
   type    = "A"
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.charlesob.domain_name_configuration[0].target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.charlesob.domain_name_configuration[0].hosted_zone_id
+    name                   = aws_cloudfront_distribution.charlesob.domain_name
+    zone_id                = aws_cloudfront_distribution.charlesob.hosted_zone_id
     evaluate_target_health = false
   }
 }
